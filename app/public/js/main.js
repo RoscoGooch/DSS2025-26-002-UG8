ea956caf7d682cab51b5641235774f9fd8ef07d4
RoscoGooch <rsgooch2004@outlook.com>
2026-04-19 17:04:42 +0100

    updated forced logout feature to allow time reset

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -6,15 +6,30 @@ async function displayUsername() {
     document.querySelector("#login_link").textContent = user_data.username;
 }
 
-minutesLeft = 3;
 //Function to timeout logins after fixed length of time
+var minutesLeft = 3;
 const showTimeoutDisplay = setTimeout(displayTimeoutMessage, 120000);
 
 function displayTimeoutMessage() {
   document.getElementById("session-timeout").removeAttribute('hidden');
 }
 
-const loginTimeout = setTimeout(forceLogout, 300000);
+document.getElementById("login_cont").addEventListener("click", function resetTimeout(){
+    minutesLeft = 3;
+    document.getElementById("timeout_display").innerHTML = "Logout in " + minutesLeft + " minutes";
+    document.getElementById("session-timeout").setAttribute('hidden', true);
+    setTimeout(displayTimeoutMessage, 120000);
+});
+
+const updateTimeout = setInterval(updateTimeoutMessage, 60000);
+
+function updateTimeoutMessage() {
+    minutesLeft = minutesLeft - 1;
+    document.getElementById("timeout_display").innerHTML = "Logout in " + minutesLeft + " minutes";
+    if (minutesLeft == 0){
+        forceLogout()
+    }
+}
 
 function forceLogout() {
     alert("Your session has expired. You have been forced to log out")
