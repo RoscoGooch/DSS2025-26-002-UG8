99d890277fd03c692ebe4b63d6cfea7b870f02c8
Finlay Skinner <62904931+Finlay-Skinner@users.noreply.github.com>
2026-04-24 18:49:20 +0100

    Changed login error messages

--- app/public/js/login.js
+++ app/public/js/login.js
@@ -4,11 +4,11 @@ async function checkLoginAttempts() {
     const form_data = await response.json();
 
     // Inform user they need to fill out the fields on the login form
-    if(form_data.username === "null" || form_data.password === "null") {
+    if (form_data.username === "null" || form_data.password === "null") {
         document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
     }
-    if(form_data.username === "" || form_data.password === "") {
-        if(document.getElementById("login_error") !== null) {
+    if (form_data.username === "" || form_data.password === "") {
+        if (document.getElementById("login_error") !== null) {
             document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
         }
 
@@ -18,30 +18,31 @@ async function checkLoginAttempts() {
         error_msg.classList.add("error");
         document.querySelector("#login_btn").parentNode.insertBefore(error_msg, document.querySelector("#login_btn"));
 
-    } else if(form_data.username !== "username") { // Inform user they have entered the incorrect username
-        if(document.getElementById("login_error") !== null) {
+        // Inform user they have entered the incorrect username or password
+    } else if (form_data.username !== "username" || form_data.password !== "password") {
+        if (document.getElementById("login_error") !== null) {
             document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
         }
 
         let error_msg = document.createElement("p");
         error_msg.id = "login_error";
-        error_msg.textContent = "Incorrect username.";
+        error_msg.textContent = "Incorrect username or password.";
         error_msg.classList.add("error");
         document.querySelector("#login_btn").parentNode.insertBefore(error_msg, document.querySelector("#login_btn"));
 
-    } else if(form_data.password !== "password") { // Inform user they have entered the incorrect password
-        if(document.getElementById("login_error") !== null) {
-            document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
-        }
+        // } else if(form_data.password !== "password") { // Inform user they have entered the incorrect password
+        //     if(document.getElementById("login_error") !== null) {
+        //         document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
+        //     }
 
-        let error_msg = document.createElement("p");
-        error_msg.id = "login_error";
-        error_msg.textContent = "Incorrect password.";
-        error_msg.classList.add("error");
-        document.querySelector("#login_btn").parentNode.insertBefore(error_msg, document.querySelector("#login_btn"));
+        //     let error_msg = document.createElement("p");
+        //     error_msg.id = "login_error";
+        //     error_msg.textContent = "Incorrect password.";
+        //     error_msg.classList.add("error");
+        //     document.querySelector("#login_btn").parentNode.insertBefore(error_msg, document.querySelector("#login_btn"));
 
     } else {
-        if(document.getElementById("login_error") !== null) {
+        if (document.getElementById("login_error") !== null) {
             document.getElementById("login_error").parentNode.removeChild(document.getElementById("login_error"));
         }
     }
